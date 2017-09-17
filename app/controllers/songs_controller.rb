97a2c7facba8fa2410f778b00d3dc2276392fd48
Anna Peterson <peterson.anna.c@gmail.com>
2017-09-16 17:20:36 -0700

    Require login in songs controller; move paragraphs inside body tags

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,28 +1,21 @@
 class SongsController < ApplicationController
+  before_action :require_login
   before_action :set_song, only: [:show, :edit, :update, :destroy]
 
-  # GET /songs
-  # GET /songs.json
   def index
     @songs = Song.all
   end
 
-  # GET /songs/1
-  # GET /songs/1.json
   def show
   end
 
-  # GET /songs/new
   def new
     @song = Song.new
   end
 
-  # GET /songs/1/edit
   def edit
   end
 
-  # POST /songs
-  # POST /songs.json
   def create
     @song = Song.new(song_params)
 
@@ -37,8 +30,6 @@ class SongsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /songs/1
-  # PATCH/PUT /songs/1.json
   def update
     respond_to do |format|
       if @song.update(song_params)
@@ -51,8 +42,6 @@ class SongsController < ApplicationController
     end
   end
 
-  # DELETE /songs/1
-  # DELETE /songs/1.json
   def destroy
     @song.destroy
     respond_to do |format|
@@ -62,13 +51,17 @@ class SongsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_song
       @song = Song.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def song_params
       params.require(:song).permit(:title, :year)
+    end
+
+    def require_login
+      unless user_signed_in?
+        redirect_to root_path, notice: 'You must be logged in to access this section.'
+      end
     end
 end
