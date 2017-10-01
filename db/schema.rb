6827f81c180bdd069a4cc85968128865337d9248
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2017-10-01 14:14:46 -0700

    Adding Genre model to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001210511) do
+ActiveRecord::Schema.define(version: 20171001211331) do
 
   create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
