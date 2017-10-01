0fa7eb92fb23c0726fe27e9d8f795bd6818c0dbc
Anna Peterson <peterson.anna.c@gmail.com>
2017-10-01 14:57:12 -0700

    Replace song_link routes with resources method

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  get 'song_links/index'
-
-  get 'song_links/new'
-
-  get 'song_links/edit'
-
-  get 'song_links/show'
-
   resources :genres
   resources :artists
   resources :songs
+  resources :song_links
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
