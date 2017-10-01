690bf89c70dd0aa0c0bbe599801dc079cbb290db
Kenneth Bidstrup <kenneth.bidstrup@gmail.com>
2017-10-01 16:21:23 -0700

    Modify song_artist controller

--- app/controllers/song_artists_controller.rb
+++ app/controllers/song_artists_controller.rb
@@ -1,10 +1,13 @@
 class SongArtistsController < ApplicationController
   before_action :require_login
+  before_action :set_song_artist, only: [:show, :edit, :update, :destroy]
 
   def index
+    @song_artists = SongArtist.all
   end
 
   def new
+    @song_artist = SongArtist.new
   end
 
   def edit
@@ -21,4 +24,13 @@ class SongArtistsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def set_song_artist
+      @song_artist = SongArtist.find(params[:id])
+    end
+
+    def song_artist_params
+      params.require(:song_artist).permit(:song_id, :artist_id)
+    end
 end
