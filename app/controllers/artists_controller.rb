058670a8f8bc9d59c1434cdb20f3f55c6d3b87d7
Anna Peterson <peterson.anna.c@gmail.com>
2017-10-01 15:20:37 -0700

    Add methods to artist controller; add content to artist views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,13 @@
 class ArtistsController < ApplicationController
+  before_action :require_login
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+
   def index
+    @artists = Artist.all
   end
 
   def new
+    @artist = Artist.new
   end
 
   def edit
@@ -10,4 +15,53 @@ class ArtistsController < ApplicationController
 
   def show
   end
+
+  def update
+    respond_to do |format|
+      if @artist.update(artist_params)
+        format.html { redirect_to @artist, notice: 'Artist was successfully updated.' }
+        format.json { render :show, status: :ok, location: @artist }
+      else
+        format.html { render :edit }
+        format.json { render json: @artist.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    respond_to do |format|
+      if @artist.save
+        format.html { redirect_to @artist, notice: 'Artist was successfully created.' }
+        format.json { render :show, status: :created, location: @artist }
+      else
+        format.html { render :new }
+        format.json { render json: @artist.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @artist.destroy
+    respond_to do |format|
+      format.html { redirect_to artists_url, notice: 'Artist was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
+
+    def artist_params
+      params.require(:artist).permit(:name)
+    end
+
+    def require_login
+      unless user_signed_in?
+        redirect_to root_path, notice: 'You must be logged in to access this section.'
+      end
+    end
 end
